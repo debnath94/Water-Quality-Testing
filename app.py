ee53f6137c95bc5b454317747663469a0efe340b
Ripan Debnath <111954951+debnath94@users.noreply.github.com>
2023-06-19 19:51:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,51 +14,20 @@ from sklearn.tree import DecisionTreeRegressor
 with open('water_quality_dt.pickle', 'rb') as file:
     model = pickle. load(file)
 
-# Create a function to preprocess the input data
-def preprocess_input(data):
-    # Preprocess the input data as needed
-    # For example, you can convert the input to a DataFrame or perform feature engineering
-    # Here, we assume the input is already in a DataFrame format
-    processed_data = data.copy()
-    return processed_data
+st.title("Water Quality Predictions")
 
-# Create a function to make predictions
+# Create input fields for user input
+temperature = st.number_input("Temperature (°C)")
+turbidity = st.number_input("Turbidity (NTU)")
+dissolved_oxygen = st.number_input("Dissolved Oxygen (mg/L)")
+conductivity = st.number_input("Conductivity (µS/cm)")
 
-def make_prediction(model, input_data):
-    #preprocess the input data
-    preprocessed_input = preprocess_input(input_data)
-    #make prediction
-    predictions = model. predict(preprocessed_input)
-    return predictions
-
-# Create the Streamlit web application
-def main():
-    st.title("Water Quality Testing")
-
-    # Create input fields for user input
-    temperature = st.number_input("Temperature (°C)")
-    turbidity = st.number_input("Turbidity (NTU)")
-    dissolved_oxygen = st.number_input("Dissolved Oxygen (mg/L)")
-    conductivity = st.number_input("Conductivity (µS/cm)")
-
-# Create a dictionary with the input data
-    input_data = {
-        "Temperature_(°C)": temperature,
-        "Turbidity_NTU": turbidity,
-        "Dissolved Oxygen_mg/L": dissolved_oxygen,
-        "Conductivity_µS/cm": conductivity
-    }
-
-# Convert the input data to a DataFrame
-    input_df = pd.DataFrame(input_data, index=[0])
-
-    # Make predictions using the model
-    predictions = make_prediction(model, input_df)
-
-    # Display the predicted pH value
-    st.subheader("Predicted pH value")
-    st.write(predictions[0])
-
+if st. button("Predict"):
+    query_point = np. array([temperature, turbidity, dissolved_oxygen, conductivity])
+    query_point = query_point. reshape(1, -1)
+    prediction = model.predict(query_point)
+    st.write("The predicted PH value is", prediction[0])
+    
 # Perform actions based on the predicted pH value
     if predictions[0] < 7:
         st.subheader("Prediction is less than 7")
